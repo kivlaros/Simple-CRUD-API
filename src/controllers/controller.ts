3899fa4abbd2312c442e69133632090f4b25b76e
Alex <kivlaros@gmail.com>
2025-05-13 01:22:55 +0300

    feat: add controllers module

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -28,3 +28,64 @@ export const putUserController = (req: IncomingMessage, res: ServerResponse) => 
     }
   });
 };
+
+export const deleteUserController = (req: IncomingMessage, res: ServerResponse) =>{
+  const id = req.url?.split('/').at(-1) as string;
+    try {
+      if (!isValidUUID(id)) {
+        throw new Error('not uuid');
+      }
+      const deletedUser = deleteUser(id);
+      res.writeHead(204, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(deletedUser));
+    } catch (err: any) {
+      if (err.message == 'not uuid') {
+        sendError(res, 400, 'not uuid');
+      } else {
+        sendError(res, 404, `${id} doesn't exist`);
+      }
+    }
+}
+
+export const postUserController = (req: IncomingMessage, res: ServerResponse)=>{
+  let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', async () => {
+      const newUser = JSON.parse(body);
+      createUser(newUser);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(newUser));
+    });
+}
+
+export const gettUserController = (req: IncomingMessage, res: ServerResponse)=>{
+  const urlArr = req.url?.split('/') as [string];
+    console.log(urlArr);
+    if (urlArr.length < 4) {
+      console.log(urlArr);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      const users = getAllUsers();
+      res.end(JSON.stringify(users));
+    } else {
+      const id = req.url?.split('/').at(-1) as string;
+      try {
+        if (!isValidUUID(id)) {
+          throw new Error('not uuid');
+        }
+        const user = getUserById(id);
+        if (!user) {
+          throw new Error();
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+      } catch (err: any) {
+        if (err.message == 'not uuid') {
+          sendError(res, 400, 'not uuid');
+        } else {
+          sendError(res, 404, `${id} doesn't exist`);
+        }
+      }
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,67 +1,15 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { getAllUsers, getUserById, createUser, deleteUser, putUser } from '../services/service.js';
-import { isValidUUID } from '../utils/validators.js';
-import { sendError } from '../utils/errorHandler.js';
-import { putUserController } from '../controllers/controller.js';
+import { putUserController, deleteUserController, postUserController, gettUserController } from '../controllers/controller.js';
 
 export const handleUsersRoute = async (req: IncomingMessage, res: ServerResponse) => {
   if (req.method === 'GET') {
-    const urlArr = req.url?.split('/') as [string];
-    console.log(urlArr);
-    if (urlArr.length < 4) {
-      console.log(urlArr);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      const users = getAllUsers();
-      res.end(JSON.stringify(users));
-    } else {
-      const id = req.url?.split('/').at(-1) as string;
-      try {
-        if (!isValidUUID(id)) {
-          throw new Error('not uuid');
-        }
-        const user = getUserById(id);
-        if (!user) {
-          throw new Error();
-        }
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(user));
-      } catch (err: any) {
-        if (err.message == 'not uuid') {
-          sendError(res, 400, 'not uuid');
-        } else {
-          sendError(res, 404, `${id} doesn't exist`);
-        }
-      }
-    }
+    gettUserController(req, res)
   }
   if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-    req.on('end', async () => {
-      const newUser = JSON.parse(body);
-      createUser(newUser);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(newUser));
-    });
+    postUserController(req, res)
   }
   if (req.method === 'DELETE') {
-    const id = req.url?.split('/').at(-1) as string;
-    try {
-      if (!isValidUUID(id)) {
-        throw new Error('not uuid');
-      }
-      const deletedUser = deleteUser(id);
-      res.writeHead(204, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(deletedUser));
-    } catch (err: any) {
-      if (err.message == 'not uuid') {
-        sendError(res, 400, 'not uuid');
-      } else {
-        sendError(res, 404, `${id} doesn't exist`);
-      }
-    }
+    deleteUserController(req, res)
   }
   if (req.method === 'PUT') {
     putUserController(req, res);
